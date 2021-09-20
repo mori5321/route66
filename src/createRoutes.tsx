db36da10ff2a32aed2a64d14b049e9a03f2aad1d
mori5321 <moriwm77@gmail.com>
2021-09-20 15:19:44 +0900

    feat: impl nest

--- src/createRoutes.tsx
+++ src/createRoutes.tsx
@@ -5,32 +5,51 @@ type Route = {
 };
 
 type Routes = {
-  [key: string]: Route;
+  [key: string]: Route | Routes;
 };
 
 type RouteFunc = (path: string) => Route;
-// type NestFunc = (path: string, callback: CreateRoutes) => Routes;
+type NestFunc = (path: string, callback: Callback) => Routes;
 
-type CreateRoutes = (
-  callback: (callbackArgs: { route: RouteFunc }) => Routes
-) => Routes;
+type Callback = (callbackArgs: { route: RouteFunc; nest: NestFunc }) => Routes;
+type CreateRoutes = (callback: Callback) => Routes;
 
 const createRoutes: CreateRoutes = (callback) => {
-  const route: RouteFunc = (path) => {
+  return _createRoutes(callback, '');
+};
+
+const _createRoutes = (callback: Callback, basePath: string) => {
+  const route: RouteFunc = (path): Route => {
+    if (basePath.length > 0) {
+      return {
+        path: basePath + path, // TODO: join URL safely
+      };
+    }
+
     return {
       path,
     };
   };
 
-  return callback({ route });
+  const nest: NestFunc = (path, callback): Routes => {
+    return _createRoutes(callback, path);
+  };
+
+  return callback({ route, nest });
 };
 
-const routes = createRoutes(({ route }) => {
+const routes = createRoutes(({ route, nest }) => {
   return {
     home: route('/home'),
+    users: nest('/users', ({ route }) => {
+      return {
+        list: route('/'),
+      };
+    }),
   };
 });
 
+// FIXME: routes.users.home みたいな型推論ができない
 console.log('Routes', routes);
 
 export { createRoutes };
